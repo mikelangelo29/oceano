8e196c08e12a7e61c75e4f3d40d3e633c217d4d2
mikelangelo29 <137167235+mikelangelo29@users.noreply.github.com>
2023-06-30 11:40:47 +0200

    Update settings.py

--- social_site/social_site/settings.py
+++ social_site/social_site/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'social_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
